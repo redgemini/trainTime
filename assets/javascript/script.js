190ae908edced280f80984906e5c5fb5b8ad23dc
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-03-15 13:46:50 -0500

    initialize firebase

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC4pXO-dUOljmnSeFCAfG2fwZn5fpt0Oxw",
+    authDomain: "train-times-22dd5.firebaseapp.com",
+    databaseURL: "https://train-times-22dd5.firebaseio.com",
+    projectId: "train-times-22dd5",
+    storageBucket: "",
+    messagingSenderId: "1005430180893"
+  };
+
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+  
