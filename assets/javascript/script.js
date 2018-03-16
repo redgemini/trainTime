5d8b81b5ac06bea0b0b20383064597b348219032
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-03-15 21:52:22 -0500

    debug

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,11 +4,28 @@
     authDomain: "train-times-22dd5.firebaseapp.com",
     databaseURL: "https://train-times-22dd5.firebaseio.com",
     projectId: "train-times-22dd5",
-    storageBucket: "",
+    storageBucket: "train-times-22dd5.appspot.com",
     messagingSenderId: "1005430180893"
   };
-
   firebase.initializeApp(config);
-  var database = firebase.database();
 
-  
+  var trainData = firebase.database();
+
+//Button Function
+$("#add-train-btn").on("click", function() {
+
+  // Pull in User Input
+  var trainName = $("#train-name-input").val().trim();
+  var destination = $("#destination-input").val().trim();
+  var firstTrain = $("#first-train-input").val().trim();
+  var frequency = $("#frequency-input").val().trim();
+
+  //Object for trainData
+  var newTrain = {
+      name: trainName,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency,
+  }
+
+})
